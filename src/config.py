1e47a13a071f7d2c1ef826113a3dedd722959821
Nouman M <nouman@192.168.1.14>
2026-03-13 19:31:41 +0530

    Changing opencode model

--- src/config.py
+++ src/config.py
@@ -40,7 +40,7 @@ OLLAMA_TEMPERATURE = 0.7
 
 OPENCODE_API_KEY = "sk-5zvOAeHrvArnaqO1BLnONrPMyYshaipvxq4LDZy4cSlbYjWVqpHP9GJEd52ZA7gF"
 OPENCODE_BASE_URL = "https://opencode.ai/zen/v1"
-OPENCODE_MODEL = "minimax-m2.5-free"
+OPENCODE_MODEL = "mimo-v2-flash-free"
 OPENCODE_TEMPERATURE = 0.7
 
 DEFAULT_LLM_PROVIDER = "opencode"
